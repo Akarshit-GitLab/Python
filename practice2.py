13281cf2a46b31a634f653e08fc1a2e7f8139a71
Akarshit-GitLab <akarshitkumar9354@gmail.com>
2025-06-30 17:09:34 +0530

    practice question 2

---
+++ practice2.py
@@ -0,0 +1,21 @@
+# side = int(input(" enter the side of a square : "))
+# area =( side * side)
+# print("area of square : ", area)
+
+
+
+# wap to get avg of 2 float number 
+
+
+# first = float(input("enter first number : "))
+# second = float(input("enter second number : "))
+# avg = (first + second)/2
+# print("average of two number : ", avg)
+
+# wap to get input 2 int number and print true of false if a is greater than b
+
+
+
+first = int(input("first number : "))
+second = int(input("second number : "))
+print(first>=second)
