03f2f7e68e45f52e8356a0174cd872ba3234a3aa
Akarshit-GitLab <akarshitkumar9354@gmail.com>
2025-07-01 08:01:09 +0530

    strings

--- string_practice1.py
+++ string_practice1.py
@@ -1,5 +1,10 @@
 #write a program to inout user name nad print its length
 
 
-name = input("enter your name : ")
-print(len(name))
+# name = input("enter your name : ")
+# print(len(name))
+
+#-------------->Wap to find occurnance of A in str
+
+str = "hi i am akarshit "
+print(str.count("a"))
