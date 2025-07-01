e6aae999f91634afe46a3f6502fc3100992a5ede
Akarshit-GitLab <akarshitkumar9354@gmail.com>
2025-07-01 08:21:54 +0530

    conditional statement

---
+++ condistional_practice.py
@@ -0,0 +1,31 @@
+# wap to check if a number entered by user is odd or even
+
+# num = int(input("enter the number : "))
+# if(num %2 ==0) :
+#     pnrint("you enter a even number")
+# else :
+#     print("you entered a odd number")
+
+
+# ------->Wap to find the greatest of three number  enterd by user<----------
+
+# num1 =int(input("enter first number : "))
+# num2 =int(input("enter second number : "))
+# num3 =int(input("enter third number : "))
+# if(num1>num2 and num1>num3):
+#     print("number 1 is greater")
+# elif(num2>num1 and num2>num3):
+#     print("number 2 is greater")
+# elif(num3>num1 and num3>num2):
+#     print("number 3 is greater")
+
+
+
+# --------->wap to check if number  multiple of 7
+
+
+num = int(input("enter the number : "))
+if(num %7 ==0) :
+    print("yes it is multiple of 7")
+else :
+    print("no it  is not multiple of 7")
