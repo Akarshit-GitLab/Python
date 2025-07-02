58789eb9e498cd9a2a871a28d546630bfc220c54
Akarshit-GitLab <akarshitkumar9354@gmail.com>
2025-07-02 08:12:38 +0530

    sorce code

---
+++ list_practice1.py
@@ -0,0 +1,8 @@
+# wap to count the number of student with grade  in  tupple
+tup = ("A","B","C","A","A","A","D")
+print(tup.count("A"))
+
+# store the above value in a list and sort them 
+list = ["A","B","C","A","A","A","D"]
+list.sort()
+print(list)
