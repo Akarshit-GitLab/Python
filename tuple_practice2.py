98cb6fb43b26136ff1b7c35c262ef4dfe8f631e2
Akarshit-GitLab <akarshitkumar9354@gmail.com>
2025-07-02 07:58:18 +0530

    tupple practice 2

---
+++ tuple_practice2.py
@@ -0,0 +1,12 @@
+# wap to check if a list contain a plaindrome of elements.
+list1 =[1,2,3,2,1]
+# list_copy =list1.copy()
+
+list_reverse =list1
+list_reverse.reverse()
+if(list1 == list_reverse):
+    print("this is plindrome")
+else:
+    print("this is not")
+
+    # we can use copy() method also
