42ef30b7c1c87f74dafa57a7f2dbbcdc4b205e47
Akarshit-GitLab <akarshitkumar9354@gmail.com>
2025-07-02 07:43:14 +0530

    tupple practice

---
+++ tuple_practice.py
@@ -0,0 +1,9 @@
+# wap to ask the user to enter names of their 3 fav movies and store them in list
+
+first = input("enter first fav movie : ")
+
+second = input("enter second fav movie : ")
+
+third = input("enter thirst fav movie : ")
+list =[first,second,third]
+print(list)
