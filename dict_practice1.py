e432a89b757c392a90060a6ebafbca6a51810381
Akarshit-GitLab <akarshitkumar9354@gmail.com>
2025-07-03 08:12:48 +0530

    source code

---
+++ dict_practice1.py
@@ -0,0 +1,22 @@
+# ---------->Store following word meanings in a python dictionary :
+# table : “a piece of furniture”,“list of facts & figures”
+# cat : “a small animal”
+
+dict = {
+    "cat" : " a small animal",
+    "table" : ["a piece of furniture","list of facts & figures"]
+}
+print(list(dict))
+
+
+# You are given a list of subjects for students. Assume one classroom is required for 1
+# subject. How many classrooms are needed by all students.
+# ”python”,"java",c++,python,javascript,java,python ,java,c++,c
+
+
+subject = {
+    "python","java","c++","python","javascript","java","python","java","c++","c"
+}
+print(len(list(subject)))
+
+
