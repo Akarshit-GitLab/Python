dbe4debaf87593319044cac82191889cf48c0b9d
Akarshit-GitLab <akarshitkumar9354@gmail.com>
2025-07-04 06:53:37 +0530

    source code

---
+++ loop_practice2.py
@@ -0,0 +1,13 @@
+# n = int(input(" enter the number : "))
+# i =1
+# for i in range(1,11,1):
+#     print( n * i )
+
+    # wap to find sum of first n number using while 
+n =5
+sum  =0
+i =0
+while(i <=n):
+    sum += i
+    i +=1
+print(sum)
