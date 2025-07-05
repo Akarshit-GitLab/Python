be777b4deae7e205e43ff9ea46449ff7bbcc4859
Akarshit-GitLab <akarshitkumar9354@gmail.com>
2025-07-05 09:25:35 +0530

    sorce code

---
+++ file_io_practice2.py
@@ -0,0 +1,17 @@
+# waf to find in which line of the file does the word "learning occur first"
+def find_word():
+    word ="learning"
+    data = True
+    line_no =1
+    with open("practice.txt" ,"r" ) as f:
+        while data:
+            data = f.readline()
+            if(word in data):
+                print("found in line number = ",line_no)
+                return
+            line_no +=1
+    return -1
+print(find_word())
+         
+        
+
