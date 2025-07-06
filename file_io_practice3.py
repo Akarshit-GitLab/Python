3beb526b7822317c1eb73f8c879013ad39738fb7
Akarshit-GitLab <akarshitkumar9354@gmail.com>
2025-07-06 06:51:26 +0530

    source code

---
+++ file_io_practice3.py
@@ -0,0 +1,9 @@
+# from a file conataining number seprated by comma, print th count of even numbers
+count =0
+with open("practice.txt" ,"r") as f:
+    data = f.read()
+    nums =data.split(",")
+    for val in nums:
+        if(int(val) %2 == 0):
+            count += 1
+print(count)
