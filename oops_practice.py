c7a99162cc80eee3efff64a94c92b0a57234d431
Akarshit-GitLab <akarshitkumar9354@gmail.com>
2025-07-06 10:16:07 +0530

    source code

---
+++ oops_practice.py
@@ -0,0 +1,18 @@
+# Create student class that takes name & marks of 3 subjects as arguments in constructor.
+# Then create a method to print the average.
+class student:
+    def __init__(self,name,marks):
+        self.name = name
+        self.marks = marks
+
+    def get_avg(self):
+        sum =0
+        for val in self.marks:
+            sum += val
+        print("hi",self.name,"you marks avg = ",sum/3)
+
+s1 =student("akarshit",[94,95,99])
+s1.get_avg()
+
+
+
