f6178a3f2355572fab91fc52602a097f0cf6cd0c
Akarshit-GitLab <akarshitkumar9354@gmail.com>
2025-07-12 21:30:08 +0530

    source code

--- file_io_practice.py
+++ file_io_practice.py
@@ -1,19 +1,19 @@
 # create a new file using python and write into it 
-# with open("practice.txt","w") as f:
-#     f.write("hi Everyone \nwe are learning java File i/o")
-#     f.write("using java \ni like programming in java")
+with open("practice.txt","w") as f:
+    f.write("hi Everyone \nwe are learning java File i/o")
+    f.write("using java \ni like programming in java")
 
 
 
 # now replace all occurance of java in python 
-# with open("practice.txt","r") as f:
-#     data = f.read()
-# new_data = data.replace("java","python")
+with open("practice.txt","r") as f:
+    data = f.read()
+new_data = data.replace("java","python")
 
-# print(new_data)
-# with open("practice.txt","w") as f:
-#     f.write(new_data)
-#     f.close()
+print(new_data)
+with open("practice.txt","w") as f:
+    f.write(new_data)
+    f.close()
 
 # waf search if the word learning exist or not 
 
