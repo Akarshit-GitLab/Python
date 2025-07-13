31b0fb3b3e28f6a6f45a95e3e1a2b4819d46ea9a
Akarshit-GitLab <akarshitkumar9354@gmail.com>
2025-07-13 20:20:27 +0530

    source code

---
+++ python_practice.py
@@ -0,0 +1,21 @@
+# 1. Print a triangle pattern of stars
+for i in range(1, 6):
+    print('*' * i)
+
+# 2. Reverse a string
+s = "hello"
+print(s[::-1])
+
+# 3. Sum of all elements in a list
+numbers = [1, 2, 3, 4, 5]
+print(sum(numbers))
+
+# 4. Check if a number is prime
+n = 29
+if n > 1:
+    for i in range(2, n):
+        if n % i == 0:
+            print(f"{n} is not prime")
+            break
+    else:
+        print(f"{n} is prime")
