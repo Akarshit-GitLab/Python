c838a1cff779ebf4eb741c0b0d91a7144464693d
Akarshit-GitLab <akarshitkumar9354@gmail.com>
2025-07-19 21:32:08 +0530

    source code

--- dict_practice1.py
+++ dict_practice1.py
@@ -11,7 +11,7 @@ print(list(dict))
 
 # You are given a list of subjects for students. Assume one classroom is required for 1
 # subject. How many classrooms are needed by all students.
-# ”python”,"java",c++,python,javascript,java,python ,java,c++,c
+# ”python”,"java",c++,python,javascript,java,python ,java,c++,c 
 
 
 subject = {
