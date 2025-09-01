db0458fabb9fc81f6a08d56fc8ee190bd77c9bc5
Akarshit-GitLab <akarshitkumar9354@gmail.com>
2025-09-01 14:12:26 +0530

    sc

--- classwork.py
+++ classwork.py
@@ -1,11 +1,22 @@
 # Prints all letters except "s" and "e"
-i =0
-a= "students"
-while i < len(a):
-    if a[i] == "s" or a[i] == "e":
-        i += 1
-        continue
-    print(a[i])
-    i += 1
+# i =0
+# a= "students"
+# while i < len(a):
+#     if a[i] == "s" or a[i] == "e":
+#         i += 1
+#         continue
+#     print(a[i])
+#     i += 1
+# ==============------------->print unicode<---------------------- 
+print(ord("b"))
+print(ord("m"))
+print(ord("s"))
+print(ord("r"))
+print(ord("c"))
+# print list and tuple
+str ="python" 
+print(tuple(str))
+print(list(str))
+print(set(str))
 
 
