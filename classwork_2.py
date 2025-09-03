7ac3ba8242801623f817070412120f29247f9778
Akarshit-GitLab <akarshitkumar9354@gmail.com>
2025-09-03 12:16:35 +0530

    sc

---
+++ classwork_2.py
@@ -0,0 +1,23 @@
+# define variables
+a = 32
+b = 6
+
+# operations
+print("Addition:", a + b)          
+print("Subtraction:", a - b)       
+print("Division:", a / b)          
+print("Exponent:", a ** b)         
+print("Remainder:", a % b)         
+print("Floor Division:", a // b)   
+# comparison operations
+print("a == b :", a == b)   # equal to
+print("a != b :", a != b)   # not equal to
+print("a > b  :", a > b)    # greater than
+print("a < b  :", a < b)    # less than
+print("a >= b :", a >= b)   # greater than or equal to
+print("a <= b :", a <= b)   # less than or equal to
+
+# assigments operators
+print("Initial a:", a)
+
+# assignment operations
