9b6495d06181f871be34937e1f6c32ee6dc26c43
Akarshit-GitLab <akarshitkumar9354@gmail.com>
2025-09-03 12:04:19 +0530

    sc

---
+++ classwork_1.py
@@ -0,0 +1,19 @@
+fruits = ["apple", "banana","cherry"]
+fruits.append("orange")
+fruits.insert(1,"grapes")
+fruits.remove("banana")
+popped = fruits.pop()
+count =fruits.count("apple")
+fruits.sort()
+fruits.reverse()
+fruits.extend(["kiwi","mango"])
+fruits.clear()
+print(fruits)
+
+# 
+var =[1,2,3,4,5,6,7,8,9]
+var.insert(3,"apple")
+var.extend(["akarshit","sunny"])
+var.count(3)
+popped =var.pop(3)
+print(popped)
